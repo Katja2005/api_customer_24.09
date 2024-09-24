fa31169027c0065e046e430b6b1331c9c71fe3b1
Katja <you@example.com>
2024-09-24 14:52:57 +0300

    it ka kaut ko rada

---
+++ app/Http/Controllers/CustomerController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use Illuminate\Http\Request;
+
+class CustomerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Customer::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Customer $customer)
+    {
+        return [
+            'customer_id' => $customer->customer_id,
+            'first_name' => $customer->first_name,
+            'last_name' => $customer->last_name,
+            'address' => $customer->adress,
+            'city' => $customer->city,
+            'state' => $customer->state,
+            'points' => $customer->points,
+            'isGoldMember' => $customer -> isGoldMember()
+        ];
+    }
+
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Customer $customer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Customer $customer)
+    {
+        //
+    }
+}
